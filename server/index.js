054e3e38d93dbbb38282a179ac2e7fd0f4362241
Korolko Anastasiia <korolkonasta@gmail.com>
2025-05-24 19:06:49 +0300

    fixed cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ const corsOptions = {
   origin: [
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://apartment-rental-platform-fpmn.vercel.app'
+    'https://apartment-rental-platform-fpmn.vercel.app',
+    'https://kind-solace-production.up.railway.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
